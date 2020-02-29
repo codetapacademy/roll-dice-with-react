77fe16863361db44bb12ffdb08c581bb9009d317
Bogdan Negura <negura.bogdan.90@gmail.com>
2020-02-29 15:56:09 +0000

    feat: [BogdanNegura] - video 2 - test for get-dice, style the button and change the dice style with a imported css style

--- src/component/dice/dice.component.jsx
+++ src/component/dice/dice.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { StyledDice } from "./dice.style";
 
 const Dice = ({ children }) => {
-  return <StyledDice>{children}</StyledDice>;
+  return <StyledDice className={`dice dice-${children}`} />;
 };
 
 export { Dice };
--- src/component/roll/roll.style.js
+++ src/component/roll/roll.style.js
@@ -1,8 +1,19 @@
 import styled from "styled-components";
 
 export const StyledRollButton = styled.button`
-  border: 2px solid blueviolet;
+  display: inline-block;
+  padding: 18px 60px;
+  border-radius: 50em;
+  position: relative;
+  z-index: 2;
+  will-change: transform, filter;
+  background: linear-gradient(to right, #52a0fd 0%, #00e2fa 80%, #00e2fa 100%);
+  color: white;
+  font-size: 3rem;
 `;
 export const StyledRollWrapper = styled.div`
   grid-column: 1/-1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
