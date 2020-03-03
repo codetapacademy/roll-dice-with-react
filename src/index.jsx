a4fdb9b7ae87167b9ddb5d120e331f18da86c436
Robert Alexandru <romaniarobertverde@yahoo.com>
2020-03-03 17:08:17 +0000

    create 2 more variables and then return all of them using interpolation.

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,12 @@
 const RollTheDice = () => {
   console.log("Soon this function will roll the dice")
   let firstDice = ~~(Math.random() * 6) + 1
-  console.log(firstDice)
+  let secondDice = ~~(Math.random() * 6) + 1
+  let thirdDice = ~~(Math.random() * 6) + 1
+  
+  return `${firstDice}${secondDice}${thirdDice}`
 }
 
-RollTheDice()
+console.log(
+  RollTheDice()
+)
