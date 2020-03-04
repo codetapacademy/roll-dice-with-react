f4132750c638c9735a3ca5876fc869847d3d6920
Robert Alexandru <romaniarobertverde@yahoo.com>
2020-03-04 12:11:24 +0000

    move the main function that generate the numbers into his own file + use it in the app component and display it into the browser with map.

--- src/component/app/app.component.jsx
+++ src/component/app/app.component.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { getDice } from '../../util'
 
 
-const RollTheDiceApp = () => <div>Let's get cracking, roll it!</div>
+const RollTheDiceApp = () => {
+  const [diceList, rollTheDice] = useState(getDice())
+  return (
+  <div>
+    <h1>
+      Let's get cracking, roll it!
+      {diceList.map(dice => <div>{dice}</div> )}
+    </h1>
+  </div>
+  )
+}
 
 export { RollTheDiceApp }
--- src/index.jsx
+++ src/index.jsx
@@ -2,17 +2,4 @@ import React from 'react'
 import { render } from 'react-dom'
 import { RollTheDiceApp } from './component/app'
 
-const RollTheDice = () => {
-  console.log("Soon this function will roll the dice")
-  let firstDice = ~~(Math.random() * 6) + 1
-  let secondDice = ~~(Math.random() * 6) + 1
-  let thirdDice = ~~(Math.random() * 6) + 1
-  
-  return `${firstDice}${secondDice}${thirdDice}`
-}
-
-console.log(
-  RollTheDice()
-)
-
 render(<RollTheDiceApp />, document.querySelector('#roll-the-dice'))
