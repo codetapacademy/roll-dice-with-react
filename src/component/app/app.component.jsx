6d9bc810d11b9278ab04c71dfd25bd66a273460b
Robert Alexandru <romaniarobertverde@yahoo.com>
2020-03-04 12:36:42 +0000

    create the Header component and import it into the app component.

--- src/component/app/app.component.jsx
+++ src/component/app/app.component.jsx
@@ -1,15 +1,14 @@
 import React, { useState } from 'react'
 import { getDice } from '../../util'
 import { StyledApp } from './app.styled'
+import { Header } from '../header'
 
 
 const RollTheDiceApp = () => {
   const [diceList, rollTheDice] = useState(getDice())
   return (
   <StyledApp>
-    <h1>
-      Let's get cracking, roll it!
-    </h1>
+    <Header titleHeader="Let's get cracking, roll it!"/>
     {diceList.map(dice => <div>{dice}</div> )}
   </StyledApp>
   )
---
+++ src/component/header/header.component.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Header = ({titleHeader}) => <h1>{titleHeader}</h1>
+
+export { Header }
